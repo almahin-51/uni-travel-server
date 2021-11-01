5ec6644367d2962d053603e001d3e653ef969a41
ALMahin-1 <almahin01751@gmail.com>
2021-11-01 23:32:12 +0600

    all Update

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run() {
 
         const database = client.db('uni_travels');
         const serviceCollection = database.collection('order_peoples');
+        const serviceCollectionTwo = database.collection('order-offer');
         
         // Get API 
         app.get('/services', async (req, res) => {
@@ -29,6 +30,29 @@ async function run() {
             res.json(services);
         });
 
+        // POST API IN MAIN OFFER
+        app.post('/services', async (req, res) => {
+            const getOffer = req.body;
+            const result = await serviceCollection.insertOne(getOffer);
+            console.log('Added success')
+            res.json(result);
+        });
+
+        // UPDATE APPROVED API
+        app.put('/my_orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const updateApproved = req.body;
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updateApproved.status,
+                }
+            };
+            const result = await serviceCollectionTwo.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
         // Get API single 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
@@ -36,7 +60,29 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const TourService = await serviceCollection.findOne(query);
             res.json(TourService);
-        })
+        });
+
+        // POST API 
+        app.post('/get_offer', async (req, res) => {
+            const getData = req.body;
+            const data = await serviceCollectionTwo.insertOne(getData);
+            res.json(data);
+        });
+
+        // GET SIGN API 
+        app.get('/my_orders', async (req, res) => {
+            const query = serviceCollectionTwo.find({});
+            const orders = await query.toArray();
+            res.json(orders);
+        });
+
+        // DELETE API 
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollectionTwo.deleteOne(query); 
+            res.json(result);
+        });
 
     }
     finally {
